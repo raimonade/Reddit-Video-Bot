1ff026ee33879f876bea7d4534bd159a4aec4077
raimonade <raimonds.korzenevskis@gmail.com>
2022-10-18 21:36:22 +0300

    req

---
+++ test.py
@@ -0,0 +1,4 @@
+import toml
+from pathlib import Path
+
+{'settings': {'allow_nsfw': False, 'theme': 'light', 'times_to_run': 1, 'opacity': 0.9, 'transition': 0.2, 'storymode': False, 'background': {'background_choice': 'minecraft'}, 'tts': {'tortoise_voice': 'train_dotrice', 'python_voice': '1', 'py_voice_num': '2', 'voice_choice': 'tortoise', 'aws_polly_voice': 'train_dotrice', 'streamlabs_polly_voice': 'Matthew', 'tiktok_voice': 'en_us_006'}}, 'reddit': {'creds': {'client_id': 'Ab3pc4CtQbEH4WdDUntxfg', 'client_secret': 'gkFXPJ_W1IiTDAPwUzY40MNhkHiKXA', 'username': 'ramkiro', 'password': 'iiDk2^2LPu$Bwr', '2fa': False}, 'thread': {'random': False, 'subreddit': 'AskReddit', 'post_id': '', 'max_comment_length': 500, 'post_lang': '', 'min_comments': 29}}}
